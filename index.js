fe4ee862048471f07c423191c06eb58998a92c53
Тимур Ахметзянов <dijer@mail.ru>
2020-12-09 21:37:33 +0300

    fix

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { Book } = require('./models');
-const e = require('express');
 const mockBooks = require('./mock/mockBooks');
 
 const store = {
