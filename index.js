1538c7898f80bd95ddeb1ad824041017fb4c6358
Тимур Ахметзянов <dijer@mail.ru>
2020-12-12 18:00:02 +0300

    netology-008

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const errorMiddleware = require('./middleware/error');
 const indexRouter = require('./routes/index');
+const booksApiRouter = require('./routes/api/books');
+const userApiRouter = require('./routes/api/user');
 const booksRouter = require('./routes/books');
-const userRouter = require('./routes/user');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
+app.set('view engine', 'ejs');
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.use('/', indexRouter);
-app.use('/api/books', booksRouter);
-app.use('/api/user', userRouter);
+app.use('/books', booksRouter);
+app.use('/api/books', booksApiRouter);
+app.use('/api/user', userApiRouter);
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
--- routes/books.js
+++ routes/books.js
@@ -1,32 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const fileMiddleware = require('../middleware/files');
 const { Book } = require('../models');
-const mockBooks = require('../mock/mockBooks');
-
-const store = {
-    books: mockBooks,
-};
+const { books } = require('../store');
 
 router.get('/', (req, res) => {
-    const { books } = store;
-    res.json(books);
+    res.render("books/index", {
+        title: "Books",
+        books,
+    });
 });
 
-router.get('/:id', (req, res) => {
-    const { books } = store;
-    const { id } = req.params;
-
-    const foundBook = books.find(book => book.id === id);
-    if (foundBook) {
-        return res.json(foundBook);
-    }
-    res.status(404);
-    res.json('Book not found!');
+router.get('/create', (req, res) => {
+    res.render("books/create", {
+        title: "Book | create",
+        book: {},
+    });
 });
 
-router.post('/', (req, res) => {
-    const { books } = store;
+router.post('/create', (req, res) => {
     const {
         title,
         description,
@@ -44,16 +35,41 @@ router.post('/', (req, res) => {
         fileCover,
         fileName,
     });
-    store.books.push(book);
+    books.push(book);
 
-    res.status(201);
-    res.json(book);
+    res.redirect('/books')
 });
 
-router.put('/:id', (req, res) => {
-    const { books } = store;
+router.get('/:id', (req, res) => {
     const { id } = req.params;
+    const foundBook = books.find(book => book.id === id);
 
+    if (foundBook) {
+        res.render("books/view", {
+            title: "Book | view",
+            book: foundBook,
+        });
+    } else {
+        res.status(404).redirect('/404');
+    }
+});
+
+router.get('/update/:id', (req, res) => {
+    const { id } = req.params;
+    const foundBook = books.find(book => book.id === id);
+
+    if (foundBook) {
+        res.render("books/update", {
+            title: "Book | view",
+            book: foundBook,
+        });
+    } else {
+        res.status(404).redirect('/404');
+    }
+});
+
+router.post('/update/:id', (req, res) => {
+    const { id } = req.params;
     const foundBook = books.find(book => book.id === id);
     if (foundBook) {
         Object.entries(req.body).forEach(([key, value]) => {
@@ -61,49 +77,23 @@ router.put('/:id', (req, res) => {
                 foundBook[key] = value;
             }
         })
-        return res.json(foundBook);
+        res.redirect(`/books/${id}`);
+    } else {
+        res.status(404).redirect('/404');
     }
-    res.status(404);
-    res.json('Book not found!');
 });
 
-router.delete('/:id', (req, res) => {
-    const { books } = store;
+router.post('/delete/:id', (req, res) => {
     const { id } = req.params;
-
     const bookIndex = books.findIndex(book => book.id === id);
+
     if (bookIndex !== -1) {
         books.splice(bookIndex, 1);
-        return res.json('ok');
-    }
-    res.status(404);
-    res.json('Book not found!');
-});
-
-router.post('/upload', fileMiddleware.single('cover-book'), (req, res) => {
-    if (req.file) {
-        const { path } = req.file;
-        res.json(path);
+        res.redirect(`/books`);
     } else {
-        res.json(null);
-    }
-});
-
-router.get('/:id/download', (req, res) => {
-    const { id } = req.params;
-    const { books } = store;
-    const foundBook = books.find(book => book.id === id);
-    if (!foundBook) {
-        res.status(404).json();
-    }
-    const { fileName, fileBook } = foundBook;
-    if (fileName && fileBook) {
-        res.download(__dirname+`/../public/books/${fileName}`, fileBook, err => {
-            if (err){
-                res.status(404).json();
-            }
-        });
+        res.status(404).redirect('/404');
     }
 });
 
 module.exports = router;
+
