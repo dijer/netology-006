d13dcd25b72f7818c07c0564bca9626d4e20616b
Тимур Ахметзянов <dijer@mail.ru>
2020-12-12 15:11:56 +0300

    add routes, download and upload files

--- mock/mockBooks.js
+++ mock/mockBooks.js
@@ -5,25 +5,28 @@ const book1 = new Book({
     authors: ['Рей Брэдбери'],
     description: 'Топ1',
     favorite: true,
-    fileCover: 'epub',
-    fileName: 'book1',
+    fileCover: '',
+    fileName: '2020-12-12T11-35-27.933Z-book.txt',
+    fileBook: 'book1.txt',
 });
 
 const book2 = new Book({
     title: '1984',
     authors: ['Джордж Оруэлл'],
     description: 'Топ2',
-    fileCover: 'pdf',
-    fileName: 'book2',
+    fileCover: '',
+    fileName: '2020-12-12T11-44-42.938Z-book.txt',
+    fileBook: 'book2.txt',
 });
 
 const book3 = new Book({
     title: 'Шантарам',
     authors: ['Грегори Дэвид Робертс'],
     description: 'Топ3',
-    fileCover: 'fb2',
-    fileName: 'book3',
+    fileCover: '',
+    fileName: '2020-12-12T11-44-52.363Z-book.txt',
     favorite: true,
+    fileBook: 'book3.txt',
 });
 
 module.exports = [book1, book2, book3];
